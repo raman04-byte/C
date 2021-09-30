ef09ff957cb6a6c2a3bf3952e533c050466f4ade
Raman <2020pgicsraman65@poornima.org>
2021-09-30 13:16:14 +0530

    new changes

--- new.cpp
+++ new.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main()
-{   cout << "Raman is learning git and github" <<endl;
+{   int columns, rows;
+
+    cout << "Enter columns " <<endl;
+    cin >> columns;
+
+    cout << "Enter rows " <<endl;
+    cin >> rows;
+
+    for(int i = 1; i <= columns; i++)
+    {
+        for(int j = 1; j <= rows; j++)
+        {
+            cout << "*";
+        }
+        cout <<endl;
+    }
     return 0;
 }
