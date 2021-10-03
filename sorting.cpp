f868a55848464f844c3729a4fb4d00134f81a0e5
raman04-byte <2020pgicsraman65@poornima.org>
2021-10-03 11:54:39 +0530

    Added sorting in file

--- sorting.cpp
+++ sorting.cpp
@@ -1,38 +1,14 @@
 #include<iostream>
-#include<math.h>
+
 #include<string>
+
 #include<conio.h>
 
 using namespace std;
 
-// int binarySearch(int array[], int n, int key)
-// {
-//     int s = 0;
-//     int e = n;
-//     while (s<= e)
-//     {
-//         int mid = (s+e)/2;
-
-//         if (array[mid]==key)
-//         {
-//             return mid;
-//         }
-//         else if (array[mid]>key)
-//         {
-//             e=mid-1;
-//         }
-//         else
-//         {
-//             s=mid+1;
-//         }
-               
-        
-//     }
-//      return -1;
-// }
-
 int main()
 {
+    
     int n;
      cin >> n;
 
