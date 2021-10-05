b5e65fcfc97c0aef95b44dc6e1017efcc110b743
raman04-byte <2020pgicsraman65@poornima.org>
2021-10-06 00:59:46 +0530

    Adding a important question answer

--- impque.cpp
+++ impque.cpp
@@ -19,6 +19,27 @@ int main()
     {
         cin >> arr[i];
     }
+    int ans = 2;
+    int pd = arr[1]-arr[0];
+    int j = 2;
+    int curr = 2;
+
+    while (j<n)
+    {
+        if (pd == arr[j]-arr[j-1])
+        {
+            curr++;
+        }
+
+        else{
+            pd = arr[j] -arr [j-1];
+            curr = 2;
+        }
+        ans = max(ans, curr);
+        j++;
+    }
+
+    cout << ans <<endl;
     
     return 0;
 }
