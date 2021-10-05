6ff7acca6242aa39a1651cd9d407352d6b32c54e
raman04-byte <2020pgicsraman65@poornima.org>
2021-10-05 22:51:53 +0530

    Problem in this file

--- subarrayandsubse.cpp
+++ subarrayandsubse.cpp
@@ -8,6 +8,28 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cout << "Enter a number " <<endl;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    } 
+
+    int curr = 0;
+    for (int i = 0; i < n; i++)
+    {
+        curr = 0;
+        for (int j = 1; j < n; j++)
+        {
+            curr+=arr[j];
+            cout << curr << endl;
+        }
+        
+    }
+    
     
     return 0;
 }
