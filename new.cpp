dfa4379e1e5e23b5d1b5f0361fad835152ee0075
raman04-byte <2020pgicsraman65@poornima.org>
2021-10-17 20:29:47 +0530

    Added some files

--- new.cpp
+++ new.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
-#include<math.h>
-#include<string>
-#include<conio.h>
+// #include<iostream>
+// #include<math.h>
+// #include<string>
+// #include<conio.h>
 
-using namespace std;
+// using namespace std;
 
 // int binarySearch(int array[], int n, int key)
-// {
+// 
 //     int s = 0;
 //     int e = n;
 //     while (s<= e)
@@ -69,36 +69,53 @@ using namespace std;
 //     return 0;
 // }
 
-class Student{
-public:
-    string name;
-    string section;
-    int year;
-    int age;
-    int height;
-    int weight;
+// class Student{
+// public:
+//     string name;
+//     string section;
+//     int year;
+//     int age;
+//     int height;
+//     int weight;
 
-    Student(string q, string w, int e, int r, int t, int y){
-        name=q;
-        section=w;
-        year=e;
-        age=r;
-        height=t;
-        weight=y;
-        }
+//     Student(string q, string w, int e, int r, int t, int y){
+//         name=q;
+//         section=w;
+//         year=e;
+//         age=r;
+//         height=t;
+//         weight=y;
+//         }
 
-void Abilities(){
-    cout<< "He/She is a very smart and speedy student";
-    cout<< "Name"<< name<<endl;
-    cout<< "Section"<< section <<endl;
-    cout<< "Year"<< year <<endl;
-    cout<< "Age"<< age <<endl;
-    cout<< "Height"<< height <<endl;
-    cout<< "Weight"<< weight<<endl;
-}
-};
+// void Abilities(){
+//     cout<< "He/She is a very smart and speedy student";
+//     cout<< "Name"<< name<<endl;
+//     cout<< "Section"<< section <<endl;
+//     cout<< "Year"<< year <<endl;
+//     cout<< "Age"<< age <<endl;
+//     cout<< "Height"<< height <<endl;
+//     cout<< "Weight"<< weight<<endl;
+// }
+// };
 
-int main(){
-    Student student_1("Reena","A",2,20,170,60);
-    student_1.Abilities();
+// int main(){
+//     Student student_1("Reena","A",2,20,170,60);
+//     student_1.Abilities();
+// }
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    srand(67);
+    for (int j = 0; j < 25; j++)
+    {
+        cout << 1+(rand()%6) <<endl;
+    }
+    
+    return 0;
 }
